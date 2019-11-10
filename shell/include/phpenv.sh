ebc3727d9c1eb14e81241405f94741fa292bf707
Naoaki Obiki <obiki.wk@gmail.com>
2019-11-10 16:29:38 +0900

    :+1: add shell provisioning script

---
+++ shell/include/phpenv.sh
@@ -0,0 +1,16 @@
+sudo apt install -y g++ \
+  libxml2 libxml2-dev \
+  libssl-dev pkg-config \
+  zlib1g-dev \
+  libbz2-dev \
+  libjpeg-dev \
+  libpng-dev \
+  libreadline-dev \
+  libtidy-dev \
+  libcurl4-openssl-dev \
+  libmcrypt-dev \
+  re2c \
+  libzip-dev \
+  libxslt1.1 libxslt-dev
+
+# anyenv install phpenv
